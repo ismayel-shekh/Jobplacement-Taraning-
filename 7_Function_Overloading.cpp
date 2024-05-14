e93c379284291c2c51b6f9e38e8448a2e32557e0
ismayel-shekh <md.shekh.ismayel@gmail.com>
2024-05-14 21:56:07 +0600

    four commit

---
+++ 7_Function_Overloading.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int add(int a, int b) {
+    int p = a + b;
+    cout << p << endl;
+    return p;
+}
+
+int add(int a, int b, int c) {
+    int p = a + b + c;
+    cout << p << endl;
+    return p;
+}
+
+int main() {
+    add(1, 3, 4);
+    return 0;
+}
