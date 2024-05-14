d1f991e459d3a5da2fb50561a3bca09f32b39253
ismayel-shekh <md.shekh.ismayel@gmail.com>
2024-05-14 22:06:31 +0600

    fifth commit

---
+++ 7_Function_Overriding.py
@@ -0,0 +1,19 @@
+class Animal:    
+    def speak(self):
+        print("Animal speaks")
+
+class Dog(Animal):
+    def speak(self):
+        print("Dog barks")
+
+class Cat(Animal):
+    def speak(self):
+        print("Cat meows")
+
+han = Animal()
+
+han.speak()
+
+dog = Dog()
+
+dog.speak()
